2b653c704e19a14e8ba9e703fd53e1cafa46e25c
nithesh <nitheshbnaik@gmail.com>
2025-11-22 12:05:24 +0530

    updated password

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Naik@123",
+  password: "*",
   database: "carbonlens",
   waitForConnections: true,
   connectionLimit: 10,
